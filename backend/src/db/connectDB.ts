862f4d8569dbfe7c62562edf31e4c6e647fb0af1
WeiHungLoh <weihung789@hotmail.com>
2025-06-09 15:01:02 +0800

    Establish pg connection for backend

---
+++ backend/src/db/connectDB.ts
@@ -0,0 +1,19 @@
+import { Pool } from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = new Pool({
+    connectionString: process.env.PG_URI
+})
+
+const connectDB = async () => {
+    try {
+        const client = await pool.connect()
+        console.log('Connected to pool')
+        client.release()
+    } catch (error) {
+        console.log('Unable to connect to pool')
+    }
+}
+
+export { pool, connectDB }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,14 @@
-const myName: string = "hey"
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './db/connectDB.ts'
+
+const startServer = async (): Promise<void> => {
+    await connectDB()
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+
+    
+}
+
+startServer()
