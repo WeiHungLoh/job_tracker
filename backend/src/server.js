bb713bc2a0b6c00e6ddec28096e8f6a10ed05a57
WeiHungLoh <weihung789@hotmail.com>
2025-06-21 15:30:47 +0800

    Update PORT number

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ const startServer = async () => {
     app.use('/interview', verifyToken, interviewRoute)
 
     const PORT = process.env.PORT || 5005;
-    app.listen(PORT, '0.0.0.0', () => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 }
