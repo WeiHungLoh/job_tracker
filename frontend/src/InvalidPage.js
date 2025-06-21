9306290203ab8271fecd95a68ec2f83f3d6d9ad2
WeiHungLoh <weihung789@hotmail.com>
2025-06-21 15:05:19 +0800

    feat: add invalid page route

---
+++ frontend/src/InvalidPage.js
@@ -0,0 +1,10 @@
+const InvalidPage = () => {
+    return(
+        <div className="invalid-page">
+            <h2>Page 404 not found</h2>
+            <a href="/addapplication">Click here to head to home page</a>
+        </div>
+    )
+}
+
+export default InvalidPage
