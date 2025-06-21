ac86cb0ca49f2a5bd6ff82de8bcb519eb2e97cc0
WeiHungLoh <weihung789@hotmail.com>
2025-06-21 15:08:36 +0800

    feat: add application and view applications

---
+++ frontend/src/JobApplication/AddApplication.js
@@ -0,0 +1,129 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Notification from '../Notification.js'
+
+const AddApplication = () => {
+    const [companyName, setCompanyName] = useState('')
+    const [jobTitle, setJobTitle] = useState('')
+    const [jobStatus, setJobStatus] = useState('Applied')
+    const [applicationDate, setApplicationDate] = useState('')
+    const [jobLocation, setJobLocation] = useState('')
+    const [jobURL, setJobURL] = useState('')
+    const navigate = useNavigate()
+    const [notification, setNotification] = useState(null)
+    const currDate = new Date(Date.now())
+
+    const isAppDatePresent = (appDate) => {
+        if (appDate === '') {
+            return currDate
+        }
+        // datetime-local displays in the format of YYYY-MM-DDThh:mm:sssZ
+        const [year, month, day, hour, minute] = appDate.split(/[-T:]/)
+        // Decrements month by 1 since month starts from 0
+        const localDate = new Date(year, month - 1, day, hour, minute)
+        return localDate
+    }
+
+    const handleAdd = async e => {
+        e.preventDefault()
+
+        if (companyName === '' || jobTitle === '') {
+            alert('Please enter company name and job title before adding a job application')
+            return
+        }
+
+        const appDate = isAppDatePresent(applicationDate)
+        
+        if (appDate > currDate) {
+            alert('Application date cannot be later than current date')
+            return
+        }
+
+        try {
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/application/add`,
+                {
+                    method: 'POST',
+                    headers:
+                    {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({ companyName, jobTitle, appDate, jobStatus, jobLocation, jobURL })
+                }
+            )
+            const message = res.text()
+            setNotification({ message, type: res.ok ? 'success' : 'error' })
+
+            // Resets assignment form after successfully adding a new assignment
+            setCompanyName('')
+            setJobTitle('')
+            setJobStatus('Applied')
+            setApplicationDate('')
+            setJobLocation('')
+            setJobURL('')
+
+            // Removes notification bar after 2 seconds
+            setTimeout(() => {
+                setNotification(null)
+            }, 2000)
+        } catch (error) {
+            alert('Failed to add an application: ' + error.message)
+        }
+    }
+
+    return (
+        <div className='add-application'>
+            <h2>Add a job application</h2>
+
+            <label>Input Company Name</label>
+            <input
+                value={companyName}
+                onChange={e => setCompanyName(e.target.value)}
+                required
+            />
+
+            <label>Input Job Title</label>
+            <input
+                value={jobTitle}
+                onChange={e => setJobTitle(e.target.value)}
+                required
+            />
+
+            <label>Input Job Status</label>
+            <select value={jobStatus} onChange={e => setJobStatus(e.target.value)}>
+                <option value='Applied'>Applied</option>
+                <option value='Interview'>Interview</option>
+                <option value='Offer'>Offer</option>
+                <option value='Rejected'>Rejected</option>
+                <option value='Accepted'>Accepted</option>
+            </select>
+
+            <label>Input Application Date (uses current date if left blank)</label>
+            <input
+                value={applicationDate}
+                onChange={e => setApplicationDate(e.target.value)}
+                type='datetime-local'
+            />
+
+            <label>Input Job Location (optional)</label>
+            <input 
+                value={jobLocation}
+                onChange={e => setJobLocation(e.target.value)}
+            />
+
+            <label>Input Job Posting URL (optional)</label>
+            <input 
+                value={jobURL}
+                onChange={e => setJobURL(e.target.value)}
+            />
+
+            <div className='submit-button'>
+                <button onClick={handleAdd}>Add Job Application</button>
+                <button onClick={() => navigate('/viewapplications')}>View Job Applications</button>
+            </div>
+            <Notification message={notification} />
+        </div>
+    )
+}
+
+export default AddApplication
---
+++ frontend/src/JobApplication/ViewApplication.js
@@ -0,0 +1,206 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import useFetchData from '../useFetchData.js'
+import DateFormatter from '../Formatter/DateFormatter.js'
+
+const ViewApplication = () => {
+    const navigate = useNavigate()
+    const { data: applications, refetch } = useFetchData(`${process.env.REACT_APP_API_URL}/application/view`)
+    const [jobStatuses, setJobStatuses] = useState({})
+    const { data: interviews } = useFetchData(`${process.env.REACT_APP_API_URL}/interview/view`)
+    const [interviewJobId, setInterviewJobId] = useState([])
+
+    useEffect(() => {
+        if (interviews) {
+            const jobIds = interviews.map(interview => interview.job_id)
+            setInterviewJobId(jobIds)
+        }
+    }, [interviews])
+
+    useEffect(() => {
+        // Obtain application.job_id from <Link> in AddInterview
+        const hash = location.hash
+        if (hash) {
+            setTimeout(() => {
+                // Ignores the first string character # to get job_id
+                const app = document.getElementById(hash.substring(1))
+                if (app) {
+                    app.scrollIntoView({ behavior: 'smooth' })
+                }
+            }, 100)
+        }
+    }, [location])
+
+    const handleDelete = async (applicationId) => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/application/${applicationId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({ applicationId })
+            })
+
+            // Refreshes UI to show remaining undeleted applications
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleDeleteAll = async () => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/application/deleteall`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                }
+            )
+
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const toggleEditStatus = async (application) => {
+        const editStatus = application.edit_status
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/application/toggleeditstatus`, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ jobId: application.job_id })
+            })
+
+            if (editStatus) {
+                await fetch(`${process.env.REACT_APP_API_URL}/application/togglejobstatus`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({
+                        jobStatus: jobStatuses[application.job_id] ?? application.job_status,
+                        jobId: application.job_id
+                    })
+                })
+            }
+
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const isEditStatus = (editStatus) => {
+        return editStatus
+    }
+
+    const isStatusInterview = (jobStatus) => {
+        if (jobStatus === 'Interview') {
+            return true
+        }
+        return false
+    }
+
+    const showAddApplicationMessage = (applications) => {
+        return applications && applications.length === 0
+    }
+
+    const showJobLocation = (field) => {
+        if (field.job_location === '') {
+            return false
+        }
+        return true
+    }
+
+    const showJobURL = (field) => {
+        if (field.job_posting_url === '') {
+            return false
+        }
+        return true
+    }
+
+    return (
+        <div className='application-list'>
+            <h2>Job Application Viewer</h2>
+
+            {showAddApplicationMessage(applications) && <div>No job application found. Start adding one now! </div>}
+
+            {applications && applications.map((application, index) => (
+                <div className='application' key={application.job_id} id={application.job_id}>
+
+                    <div className='application-content'>
+                        <h2>{index + 1}. {application.company_name}</h2>
+                        <p>Job Title: {application.job_title}</p>
+                        {showJobLocation(application) &&
+                            <p>Location: {application.job_location}</p>}
+                        <p className='date'>Application Date: {DateFormatter(application.application_date).formattedDate}</p>
+                        <p>Time since application: {DateFormatter(application.application_date).timeSinceApplication}</p>
+                        <p className={
+                            application.job_status === 'Applied'
+                                ? 'applied' : application.job_status === 'Interview'
+                                    ? 'interview' : application.job_status === 'Offer'
+                                        ? 'offer' : application.job_status === 'Rejected'
+                                            ? 'rejected' : 'accepted'
+                        }>Job Status: {application.job_status}</p>
+
+                        {isEditStatus(application.edit_status) && <select
+                            value={jobStatuses[application.job_id] ?? application.job_status}
+                            onChange={e =>
+                                setJobStatuses(app => ({
+                                    ...app,
+                                    [application.job_id]: e.target.value
+                                }))
+                            }
+                        >
+                            <option value='Applied' disabled=
+                                {interviewJobId.includes(application.job_id)}>
+                                Applied
+                            </option>
+                            <option value='Interview'>Interview</option>
+                            <option value='Offer'>Offer</option>
+                            <option value='Accepted'>Accepted</option>
+                            <option value='Rejected'>Rejected</option>
+                        </select>}
+
+                        {isStatusInterview(application.job_status) &&
+                            <Link to='/addinterview' state={{
+                                app: application
+                            }}>
+                                Click here to add an interview
+                            </Link>
+                        }
+                        {showJobURL(application) &&
+                            <a className='url' href={application.job_posting_url} target='_blank'>
+                                Click here to head to job application URL
+                            </a>}
+                    </div>
+
+                    <div className='button-group'>
+                        <button onClick={() => toggleEditStatus(application)}>
+                            {isEditStatus(application.edit_status) ? 'Save Changes' : 'Edit Status'}
+                        </button>
+
+                        <button onClick={() => handleDelete(application.job_id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+
+            <div className='application-button'>
+                <button onClick={() => navigate('/addapplication')}>Add new application</button>
+                <button onClick={() => handleDeleteAll()}>Delete all applications</button>
+            </div>
+        </div>
+    )
+}
+
+export default ViewApplication
