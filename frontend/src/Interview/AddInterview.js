7703fc98e2ba653a42079942b2bf82464ba3c06a
WeiHungLoh <weihung789@hotmail.com>
2025-06-21 15:08:26 +0800

    feat: add interview and view interviews

---
+++ frontend/src/Interview/AddInterview.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useLocation, Navigate, Link } from 'react-router-dom'
+import Notification from '../Notification.js'
+
+const AddInterview = () => {
+    const [interviewDate, setInterviewDate] = useState('')
+    const [interviewLocation, setInterviewLocation] = useState('')
+    const [interviewType, setInterviewType] = useState('')
+    const [notes, setNotes] = useState('')
+    const navigate = useNavigate()
+    const [notification, setNotification] = useState(null)
+    const location = useLocation()
+    // Receives the state that has been passed when user clicks 'Click here to add an interview' button
+    const app = location.state?.app
+
+    // Forbids users from adding interview without clicking 'Click here to add an interview' button
+    if (!app) {
+        return <Navigate to='/viewapplications' />
+    }
+
+    // datetime-local displays in the format of YYYY-MM-DDThh:mm:sssZ
+    const [year, month, day, hour, minute] = interviewDate.split(/[-T:]/)
+    // Decrements month by 1 since month starts from 0
+    const localDate = new Date(year, month - 1, day, hour, minute)
+
+    const handleAdd = async e => {
+        e.preventDefault()
+
+        if (interviewDate === '' || interviewLocation === '') {
+            alert('Please enter date and location before adding an interview')
+            return
+        }
+
+        try {
+            const res = await fetch(`${process.env.REACT_APP_API_URL}/interview/add`,
+                {
+                    method: 'POST',
+                    headers:
+                    {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({ jobId: app.job_id, interviewDate: localDate, interviewLocation, interviewType, notes })
+                }
+            )
+            const message = await res.text()
+            setNotification({ message, type: res.ok ? 'success' : 'error' })
+
+            // Resets interview form after successfully adding a new interview
+            setInterviewDate('')
+            setInterviewLocation('')
+            setInterviewType('')
+            setNotes('')
+
+            // Removes notification bar after 2 seconds
+            setTimeout(() => {
+                setNotification(null)
+            }, 2000)
+        } catch (error) {
+            alert('Failed to add an application: ' + error.message)
+        }
+    }
+
+    return (
+        <div className='add-interview'>
+            <h2>
+                You are adding an interview for the position of {app.job_title} at {app.company_name}
+            </h2>
+
+            <label>Input Interview Date</label>
+            <input
+                value={interviewDate}
+                onChange={e => setInterviewDate(e.target.value)}
+                type='datetime-local'
+                required
+            />
+
+            <label>Input Interview Location</label>
+            <input
+                value={interviewLocation}
+                onChange={e => setInterviewLocation(e.target.value)}
+                required
+                placeholder='E.g. Zoom'
+            />
+
+            <label>Input Interview Type (optional)</label>
+            <input
+                value={interviewType}
+                onChange={e => setInterviewType(e.target.value)}
+            />
+
+            <label>Input Additional Notes (optional)</label>
+            <input
+                value={notes}
+                onChange={e => setNotes(e.target.value)}
+            />
+
+            <div className='submit-button'>
+                <button onClick={handleAdd}>Add Interview</button>
+                <button onClick={() => navigate('/viewinterviews')}>View Interviews</button>
+                <Link to={`/viewapplications#${app.job_id}`}>Back</Link>
+            </div>
+            <Notification message={notification} />
+        </div>
+    )
+}
+
+export default AddInterview
---
+++ frontend/src/Interview/ViewInterview.js
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import useFetchData from '../useFetchData.js'
+import DateFormatter from '../Formatter/DateFormatter.js'
+
+const ViewInterview = () => {
+    const navigate = useNavigate()
+    const { data: interviews, refetch } = useFetchData(`${process.env.REACT_APP_API_URL}/interview/view`)
+
+    const handleDelete = async (interviewId) => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/interview/${interviewId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            // Refreshes UI to show remaining undeleted interviews
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleDeleteAll = async () => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/interview/deleteall`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                }
+            )
+
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const showAddinterviewMessage = (interviews) => {
+        return interviews && interviews.length === 0
+    }
+
+    const showInterviewType = (field) => {
+        if (field.interview_type === '') {
+            return false
+        }
+        return true
+    }
+
+    const showInterviewNotes = (field) => {
+        if (field.notes === '') {
+            return false
+        }
+        return true
+    }
+
+
+    return (
+        <div className='interview-list'>
+            <h2>Job Interview Viewer</h2>
+
+            {showAddinterviewMessage(interviews) && <div>No job interview found. Start adding one now! </div>}
+
+            {interviews && interviews.map((interview, index) => (
+                <div className='interview' key={interview.interview_id}>
+
+                    <div className='interview-content'>
+                        <h2>{index + 1}. {interview.company_name}</h2>
+                        <p>Job Title: {interview.job_title}</p>
+                        <p>Location: {interview.interview_location}</p>
+                        {showInterviewType(interview) &&
+                            <p>Interview Type: {interview.interview_type}</p>}
+                        {showInterviewNotes(interview) &&
+                            <p>Notes: {interview.notes}</p>}
+                        <p className='date'>Interview Date: {DateFormatter(interview.interview_date).formattedDate}</p>
+                        <p>Time left: {DateFormatter(interview.interview_date).timeBeforeInterview}</p>
+                        <Link to={`/viewapplications#${interview.job_id}`}>
+                            Click here to review corresponding job application
+                        </Link>
+                    </div>
+
+                    <div className='button-group'>
+                        <button onClick={() => handleDelete(interview.interview_id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+
+            <div className='interview-button'>
+                <button onClick={() => navigate('/addinterview')}>Add new interview</button>
+                <button onClick={() => handleDeleteAll()}>Delete all interviews</button>
+            </div>
+        </div>
+    )
+}
+
+export default ViewInterview
