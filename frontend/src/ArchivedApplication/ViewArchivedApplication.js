f7ce016755efa8834bad2507448678255a956e3a
WeiHungLoh <weihung789@hotmail.com>
2025-06-22 16:36:48 +0800

    feat: add frontend for archived interviews and applications

---
+++ frontend/src/ArchivedApplication/ViewArchivedApplication.js
@@ -0,0 +1,145 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import useFetchData from '../useFetchData.js'
+import DateFormatter from '../Formatter/DateFormatter.js'
+
+const ViewArchivedApplication = () => {
+    const { data: archivedApplications, refetch } = useFetchData(`${process.env.REACT_APP_API_URL}/archivedapplication/view`)
+    const location = useLocation()
+
+    useEffect(() => {
+        // Obtains application.job_id from <Link> in AddInterview
+        const hash = location.hash
+        if (hash) {
+            setTimeout(() => {
+                // Ignores the first string character # to get job_id
+                const app = document.getElementById(hash.substring(1))
+                if (app) {
+                    app.scrollIntoView({ behavior: 'smooth' })
+                }
+            }, 100)
+        }
+    }, [location])
+
+    const handleDelete = async (archivedApplicationId) => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/archivedapplication/${archivedApplicationId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+            })
+
+            // Refreshes UI to show remaining undeleted archivedApplications
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleDeleteAll = async () => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/archivedapplication/deleteall`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                }
+            )
+
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleUnarchive = async (archivedJobId) => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/archivedapplication/unarchive`,
+                {
+                    method: 'POST',
+                    headers:
+                    {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    },
+                    body: JSON.stringify({ archivedJobId })
+                }
+            )
+            refetch()
+        } catch (error) {
+            alert('Failed to archive an application ' + error.message)
+        }
+    }
+
+    const showArchiveApplicationMessage = (archivedApplications) => {
+        return archivedApplications && archivedApplications.length === 0
+    }
+
+    const showJobLocation = (field) => {
+        if (field.job_location === '') {
+            return false
+        }
+        return true
+    }
+
+    const showJobURL = (field) => {
+        if (field.job_posting_url === '') {
+            return false
+        }
+        return true
+    }
+
+    return (
+        <div className='archived-application-list'>
+            <h2>Archived Job Application Viewer</h2>
+
+            {showArchiveApplicationMessage(archivedApplications) && <div>No archived job application found. Start archiving now! </div>}
+
+            {archivedApplications && archivedApplications.map((application, index) => (
+                <div className='application' key={application.job_id} id={application.job_id}>
+
+                    <div className='application-content'>
+                        <h2>{index + 1}. {application.company_name}</h2>
+                        <p>Job Title: {application.job_title}</p>
+                        {showJobLocation(application) &&
+                            <p>Location: {application.job_location}</p>}
+                        <p className='date'>Application Date: {DateFormatter(application.application_date).formattedDate}</p>
+                        <p>Time since application: {DateFormatter(application.application_date).timeSinceApplication}</p>
+                        <p className={
+                            application.job_status === 'Applied'
+                                ? 'applied' : application.job_status === 'Interview'
+                                    ? 'interview' : application.job_status === 'Offer'
+                                        ? 'offer' : application.job_status === 'Rejected'
+                                            ? 'rejected' : 'accepted'
+                        }>Job Status: {application.job_status}</p>
+
+                        {showJobURL(application) &&
+                            <a className='url' href={application.job_posting_url} target='_blank'>
+                                Click here to head to job application URL
+                            </a>}
+                    </div>
+
+                    <div className='button-group'>
+                        <div className='unarchive-button' onClick={() => handleUnarchive(application.archived_job_id)}>
+                            <button>
+                                Unarchive
+                            </button>
+                        </div>
+                        
+                        <button onClick={() => handleDelete(application.archived_job_id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+
+            <div className='application-button'>
+                <button onClick={() => handleDeleteAll()}>Delete all archived applications</button>
+            </div>
+        </div>
+    )
+}
+
+export default ViewArchivedApplication
---
+++ frontend/src/ArchivedApplication/ViewArchivedInterview.js
@@ -0,0 +1,99 @@
+import { Link } from 'react-router-dom'
+import useFetchData from '../useFetchData.js'
+import DateFormatter from '../Formatter/DateFormatter.js'
+
+const ViewArchivedInterview = () => {
+    const { data: archivedInterviews, refetch } = useFetchData(`${process.env.REACT_APP_API_URL}/archivedinterview/view`)
+
+    const handleDelete = async (interviewId) => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/archivedinterview/${interviewId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            // Refreshes UI to show remaining undeleted interviews
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleDeleteAll = async () => {
+        try {
+            await fetch(`${process.env.REACT_APP_API_URL}/archivedinterview/deleteall`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                }
+            )
+
+            refetch()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const showArchiveInterviewMessage = (archivedInterviews) => {
+        return archivedInterviews && archivedInterviews.length === 0
+    }
+
+    const showInterviewType = (field) => {
+        if (field.interview_type === '') {
+            return false
+        }
+        return true
+    }
+
+    const showInterviewNotes = (field) => {
+        if (field.notes === '') {
+            return false
+        }
+        return true
+    }
+
+
+    return (
+        <div className='archived-interview-list'>
+            <h2>Archived Job Interview Viewer</h2>
+
+            {showArchiveInterviewMessage(archivedInterviews) && <div>No archived job interview found. Start archiving now! </div>}
+
+            {archivedInterviews && archivedInterviews.map((interview, index) => (
+                <div className='interview' key={interview.interview_id}>
+
+                    <div className='interview-content'>
+                        <h2>{index + 1}. {interview.company_name}</h2>
+                        <p>Job Title: {interview.job_title}</p>
+                        <p>Location: {interview.interview_location}</p>
+                        {showInterviewType(interview) &&
+                            <p>Interview Type: {interview.interview_type}</p>}
+                        {showInterviewNotes(interview) &&
+                            <p>Notes: {interview.notes}</p>}
+                        <p className='date'>Interview Date: {DateFormatter(interview.interview_date).formattedDate}</p>
+                        <p>Time left: {DateFormatter(interview.interview_date).timeBeforeInterview}</p>
+                        <Link to={`/viewapplications#${interview.job_id}`}>
+                            Click here to review corresponding job application
+                        </Link>
+                    </div>
+
+                    <div className='button-group'>
+                        <button onClick={() => handleDelete(interview.archived_interview_id)}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            ))}
+
+            <div className='interview-button'>
+                <button onClick={() => handleDeleteAll()}>Delete all archived interviews</button>
+            </div>
+        </div>
+    )
+}
+
+export default ViewArchivedInterview
