5afafb16b3d708c7cfdae8eff0fc53bf9ef739d5
WeiHungLoh <weihung789@hotmail.com>
2025-06-22 16:35:15 +0800

    Create PSQL tables for archived applications and interviews

--- backend/src/db/queries/createTable.js
+++ backend/src/db/queries/createTable.js
@@ -36,10 +36,35 @@ const createTable = async () => {
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`
 
+    const createArchivedApplicationTable =
+        `CREATE TABLE IF NOT EXISTS archived_job_applications (
+            archived_job_id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
+            company_name TEXT NOT NULL,
+            job_title TEXT NOT NULL,
+            application_date TIMESTAMPTZ,
+            job_status TEXT CHECK (job_status IN ('Applied', 'Interview', 'Offer', 'Rejected', 'Accepted')),
+            job_location TEXT,
+            job_posting_url TEXT
+        )`
+
+    const createArchivedInterviewTable =
+        `CREATE TABLE IF NOT EXISTS archived_interviews (
+            archived_interview_id SERIAL PRIMARY KEY,
+            archived_job_id INTEGER REFERENCES archived_job_applications(archived_job_id) ON DELETE CASCADE,
+            user_id INTEGER REFERENCES users(user_id),
+            interview_date TIMESTAMPTZ NOT NULL,
+            interview_location TEXT NOT NULL,
+            interview_type TEXT,
+            notes TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`
     try {
         await pool.query(createUsersTable)
         await pool.query(createJobAppTable)
         await pool.query(createInterviewTable)
+        await pool.query(createArchivedApplicationTable)
+        await pool.query(createArchivedInterviewTable)
     } catch (error) {
         {
             console.log('Unable to create table ' + error.message)
