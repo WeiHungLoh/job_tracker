c26d5aecdea7a42dbd3b8c2584ac7d94a372dd3a
WeiHungLoh <weihung789@hotmail.com>
2025-06-22 16:37:01 +0800

    feat: add toggle button to hide and unhide archive button

---
+++ frontend/src/Icons/ToggleButton.js
@@ -0,0 +1,19 @@
+import './ToggleButton.css'
+
+const ToggleButton = ( {toggled, onToggle }) => {
+    const buttonMessage = (toggled) => {
+        return toggled ? 'Hide Archive Button' : 'Unhide Archive Button'
+    }
+
+    return (
+        <div className='hide-archive-button'>
+            <div>{buttonMessage(toggled)}</div>
+            <button className={`toggle-button ${toggled ? 'active' : ''}`}
+                onClick={onToggle}>
+                <div className='thumb' />    
+            </button>
+        </div>
+    )
+}
+
+export default ToggleButton
