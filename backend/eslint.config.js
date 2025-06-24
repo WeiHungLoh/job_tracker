a7f976f3513d7a4bc8e809d7240b07e90c071b4e
WeiHungLoh <weihung789@hotmail.com>
2025-06-24 15:37:15 +0800

    test: Add eslint config to backend to ensure checkstyle enforcement

---
+++ backend/eslint.config.js
@@ -0,0 +1,37 @@
+// Taken from: https://eslint.org/docs/latest/use/configure/configuration-files
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+	{
+		files: ['**/*.js'],
+		plugins: {
+			js
+		},
+    languageOptions: {
+      // ignores errors pertaining to 'console' or 'process' not defined
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+		rules: {
+      ...js.configs.recommended.rules,
+      // [rule severity, style to enforce]
+			'no-unused-vars': 'warn',
+      'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+      'semi': ['error', 'never'],
+      'no-trailing-spaces': 'error',
+      'space-before-function-paren': ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+       'sort-imports': ['error', {
+        'ignoreCase': false,
+        'ignoreDeclarationSort': false,
+        'ignoreMemberSort': false,
+        'memberSyntaxSortOrder': ['none', 'all', 'multiple', 'single'],
+        'allowSeparatedGroups': false
+      }]
+		},
+	},
+])
