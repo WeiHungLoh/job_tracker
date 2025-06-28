49257d53ca375299b63a8500329d30792cdc6fb8
WeiHungLoh <weihung789@hotmail.com>
2025-06-29 00:02:06 +0800

    test: add AddApplication form submission and validation tests

--- frontend/src/JobApplication/AddApplication.test.js
+++ frontend/src/JobApplication/AddApplication.test.js
@@ -1,15 +1,50 @@
-import { render, screen } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
+import { render, screen, waitFor } from '@testing-library/react'
 import AddApplication from './AddApplication.js'
+import { MemoryRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
-describe('Test for add application component', () => {
-    test('Test for header', () => {
+global.fetch = jest.fn()
+global.alert = jest.fn()
+
+describe('User add application flow', () => {
+    // Resets the state of fetch and mockNavigate
+    beforeEach(() => {
+        fetch.mockReset()
+        alert.mockReset()
+    })
+
+    test('successfully adds an application and a notification message is shown', async () => {
+        global.fetch.mockResolvedValueOnce({
+            ok: true,
+            status: 201,
+            text: async () => 'Successfully added a job application!'
+        })
+
         render(
             <MemoryRouter>
                 <AddApplication />
             </MemoryRouter>
         )
-        const headerMessage = screen.getByText('Add a job application')
-        expect(headerMessage).toBeInTheDocument()
+
+        userEvent.type(screen.getByLabelText(/input company name/i), 'ABC Pte Ltd')
+        userEvent.type(screen.getByLabelText(/input job title/i), 'Cleaner')
+        userEvent.click(screen.getByRole('button', { name: /add job application/i }))
+
+        await waitFor(() => expect(fetch).toHaveBeenCalled())
+        await waitFor(() => expect(screen.getByTestId('noti')).toBeInTheDocument(), { timeOut: 2500 })
+    })
+
+    test('shows alert message due to company name not filled in', async () => {
+        render(
+            <MemoryRouter>
+                <AddApplication />
+            </MemoryRouter>
+        )
+
+        userEvent.type(screen.getByLabelText(/input job title/i), 'Cleaner')
+        userEvent.click(screen.getByRole('button', { name: /add job application/i }))
+
+        await waitFor(() => expect(alert)
+            .toHaveBeenCalledWith('Please enter company name and job title before adding a job application'))
     })
 })
