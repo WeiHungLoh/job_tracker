51cae33b52e0a8aa1830a85d6724ab7180969bc5
WeiHungLoh <weihung789@hotmail.com>
2025-10-08 22:56:42 +0800

    fix: change declined to upper case

--- server/src/db/queries/jobApplications.js
+++ server/src/db/queries/jobApplications.js
@@ -17,7 +17,7 @@ const getJobApplications = async (userId) => {
             CASE 
                 WHEN job_status = 'Accepted' THEN 1
                 WHEN job_status = 'Offer' THEN 2
-                WHEN job_status = 'Declined' THEN 3
+                WHEN UPPER(job_status) = 'DECLINED' THEN 3
                 WHEN job_status = 'Interview' THEN 4
                 WHEN job_status = 'Applied' THEN 5
                 WHEN job_status = 'Ghosted' THEN 6
