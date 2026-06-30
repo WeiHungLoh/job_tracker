7b3860236c6b55d52cda8e8c5a0371e6f7b35fac
WeiHungLoh <weihung789@gmail.com>
2026-06-30 16:06:21 +0800

    use lazy import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,99 @@
-import { type RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import {
+    type RouteObject,
+    RouterProvider,
+    createBrowserRouter,
+} from 'react-router-dom';
 import AddApplication from './pages/application/jobApplication/addApplication/AddApplication';
 import AddInterview from './pages/interview/jobInterview/addInterview/AddInterview';
-import Dashboard from './pages/dashboard/Dashboard';
 import FallbackScreen from './components/fallbackScreen/FallbackScreen';
 import InvalidPage from './pages/invalidPage/InvalidPage';
 import ProtectedLayout from './components/protectedLayout/ProtectedLayout';
 import ProtectedRoutes from './components/protectedRoutes/ProtectedRoutes';
 import SignIn from './pages/authentication/signIn/SignIn';
-import SignUp from './pages/authentication/signUp/SignUp';
-import UserGuide from './pages/userGuide/UserGuide';
 import ViewApplication from './pages/application/jobApplication/viewApplication/ViewApplication';
 import ViewArchivedApplication from './pages/application/archivedApplication/viewArchivedApplication/ViewArchivedApplication';
 import ViewArchivedInterview from './pages/interview/archivedInterview/viewArchivedInterview/ViewArchivedInterview';
 import ViewInterview from './pages/interview/jobInterview/viewInterview/ViewInterview';
 import { routes } from './routes';
 
+const Dashboard = lazy(
+    () => import('./pages/dashboard/Dashboard')
+);
+
+const SignUp = lazy(
+    () => import('./pages/authentication/signUp/SignUp')
+);
+
+const UserGuide = lazy(
+    () => import('./pages/userGuide/UserGuide')
+);
+
 const reloadPage = () => window.location.reload();
 
 export const appRoutes: RouteObject[] = [
     {
-        errorElement: <FallbackScreen variant='authenticationError' onAction={reloadPage} />,
+        errorElement: (
+            <FallbackScreen
+                variant='authenticationError'
+                onAction={reloadPage}
+            />
+        ),
         children: [
-            { path: routes.signIn, element: <SignIn /> },
-            { path: routes.signUp, element: <SignUp /> },
-            { path: routes.userGuide, element: <UserGuide /> },
+            {
+                path: routes.signIn,
+                element: <SignIn />,
+            },
+            {
+                path: routes.signUp,
+                element: <SignUp />,
+            },
+            {
+                path: routes.userGuide,
+                element: <UserGuide />,
+            },
             {
                 element: <ProtectedRoutes />,
                 children: [
                     {
                         element: <ProtectedLayout />,
                         children: [
-                            { path: routes.dashboard, element: <Dashboard /> },
-                            { path: routes.addApplication, element: <AddApplication /> },
-                            { path: routes.viewApplications, element: <ViewApplication /> },
-                            { path: routes.addInterview, element: <AddInterview /> },
-                            { path: routes.viewInterviews, element: <ViewInterview /> },
-                            { path: routes.archivedApplications, element: <ViewArchivedApplication /> },
-                            { path: routes.archivedInterviews, element: <ViewArchivedInterview /> },
+                            {
+                                path: routes.dashboard,
+                                element: <Dashboard />,
+                            },
+                            {
+                                path: routes.addApplication,
+                                element: <AddApplication />,
+                            },
+                            {
+                                path: routes.viewApplications,
+                                element: <ViewApplication />,
+                            },
+                            {
+                                path: routes.addInterview,
+                                element: <AddInterview />,
+                            },
+                            {
+                                path: routes.viewInterviews,
+                                element: <ViewInterview />,
+                            },
+                            {
+                                path: routes.archivedApplications,
+                                element: <ViewArchivedApplication />,
+                            },
+                            {
+                                path: routes.archivedInterviews,
+                                element: <ViewArchivedInterview />,
+                            },
                         ],
                     },
                 ],
             },
-            { path: '*', element: <InvalidPage /> },
+            {
+                path: '*',
+                element: <InvalidPage />,
+            },
         ],
     },
 ];
@@ -49,7 +101,11 @@ export const appRoutes: RouteObject[] = [
 const router = createBrowserRouter(appRoutes);
 
 const App = () => {
-    return <RouterProvider router={router} />;
+    return (
+        <Suspense fallback={null}>
+            <RouterProvider router={router} />
+        </Suspense>
+    );
 };
 
 export default App;
